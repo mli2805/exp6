8a8306e8ab0abd7ab8b8ccc53cc59d67b1b7b88a
mli2805 <mli2805@gmail.com>
2023-12-11 13:34:40 +0300

    +markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [
@@ -10,10 +12,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      sanitize: SecurityContext.NONE,
+    }),
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/assets/file2.md
@@ -0,0 +1,28 @@
+## **Help tooltip caption**
+
+---
+
+```typescript
+this.ref.instance.section = 'text текст';
+```
+
+---
+
+<img src=https://upload.wikimedia.org/wikipedia/commons/f/f8/Whole-Lemon.jpg width=150 />
+<img src="/assets/Whole-Lemon.jpg" width=150 />
+
+images could be loaded from assets
+
+> Using markdown component and/or directive, you `will` be able to use the lineNumbers property to activate the plugin. <span class="blue-text">The property can be</span> used in combination with either data for variable binding, src for remote content or using transclusion for static markdown.
+
+> Additionally, you can use start input property to specify the offset number for the first display line.
+
+```css
+.blue-text {
+color: blue;
+}
+```
+
+Link to the official page of **Markdown** on github:
+
+<a href="https://github.com/jfcere/ngx-markdown" target="_blank">ngx-markdown</a>
