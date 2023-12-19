59284d0448d243cf103e85049dccd1d658e54460
mli2805 <mli2805@gmail.com>
2023-12-19 12:35:14 +0300

    appColorize fix

--- src/app/colorize.directive.ts
+++ src/app/colorize.directive.ts
@@ -4,7 +4,7 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
   selector: '[appColorize]'
 })
 export class ColorizeDirective {
-  @Input() set rtuColorizeBySystemEventLevel(color: string) {
+  @Input() set appColorize(color: string) {
     this.applyColor(color);
   }
   constructor(private el: ElementRef, private renderer: Renderer2) { }
@@ -12,6 +12,7 @@ export class ColorizeDirective {
   private applyColor(color: string){
     const { nativeElement } = this.el;
 
-    this.renderer.addClass(nativeElement, `bg-${color}-600`);
+    this.renderer.setStyle(nativeElement, 'background', color);
+    // this.renderer.addClass(nativeElement, `bg-${color}-600`);
   }
 }
